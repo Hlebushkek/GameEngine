c5632016cf1b885caf76c874856e34ba71531870
Hlebushkek <volsoor@gmail.com>
2023-07-24 02:00:06 -0700

    Update GameObject rotation system, add front, up and right. Camera now inherit GameObject

--- include/GameObject.hpp
+++ include/GameObject.hpp
@@ -13,32 +13,45 @@
 
 namespace Engine
 {
-    class ENGINE_API GameObject
-    {
-    public:
-        GameObject(glm::vec3 position = glm::vec3(0.f), glm::vec3 rotation = glm::vec3(0.f), glm::vec3 scale = glm::vec3(1.f));
 
-        void setPosition(const glm::vec3& position);
-        glm::vec3 getPosition();
-        void setRotation(const glm::vec3& rotation);
-        void setScale(const glm::vec3& scale);
+class ENGINE_API GameObject
+{
+public:
+    GameObject(glm::vec3 position = glm::vec3(0.f), glm::vec3 rotation = glm::vec3(0.f), glm::vec3 scale = glm::vec3(1.f));
 
-        void move(const glm::vec3& position);
-        void rotate(const glm::vec3& rotation);
-        void scaleUp(const glm::vec3& scale);
-        
-        void virtual Render(Shader* shader);
+    void setPosition(const glm::vec3& position);
+    glm::vec3 getPosition();
+    void setRotation(const glm::vec3& rotation);
+    void setScale(const glm::vec3& scale);
+
+    void move(const glm::vec3& position);
+    void rotate(const glm::vec3& rotation);
+    void scaleUp(const glm::vec3& scale);
     
-    protected:
-        glm::vec3 position;
-        glm::vec3 rotation;
-        glm::vec3 scale;
+    void virtual Render(Shader* shader);
 
-        glm::mat4 ModelMatrix;
+    static const glm::vec3 WORLD_FRONT; // Todo: move it to some kind of world settings class
+    static const glm::vec3 WORLD_UP; // Todo: move it to some kind of world settings class
 
-        std::vector<Mesh*> meshes;
-        
-        void updateUniforms(Shader* shader);
-        void updateModelMatrix();
-    };
-} // namespace Engine
+    //Virtual
+    virtual void Update() {}
+
+protected:
+    glm::vec3 position;
+    glm::vec3 rotation;
+    glm::vec3 scale;
+
+    glm::vec3 front;
+    glm::vec3 right;
+    glm::vec3 up;
+
+    glm::mat4 ModelMatrix;
+
+    std::vector<Mesh*> meshes;
+    
+    void updateUniforms(Shader* shader);
+    void updateDirections();
+    void updateModelMatrix();
+};
+
+}
